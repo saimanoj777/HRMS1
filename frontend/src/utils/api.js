dcc071c3fdfcc0475f6c763a5fb8fb0f25b483cb
saimanoj777 <saimanoj2327@gmail.com>
2025-11-24 01:45:17 +0530

    api changed

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
-  // baseURL: "https://hrms-backend-w91k.onrender.com/api" // Direct connection to backend API
+  baseURL: "https://hrms-backend-w91k.onrender.com/api" // Direct connection to backend API
 })
